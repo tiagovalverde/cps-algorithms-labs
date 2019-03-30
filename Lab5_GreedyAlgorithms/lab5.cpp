b6b4f2a08a88b68aa2398ffdd8a2b0b795478116
Tiago Valverde <tiago.c.v17@gmail.com>
2019-03-29 20:52:39 -0400

    lab 5 completed and README updated

--- Lab5_GreedyAlgorithms/lab5.cpp
+++ Lab5_GreedyAlgorithms/lab5.cpp
@@ -1,65 +1,127 @@
-/*
-============= APPLICATION GOAL ==============
-Running a motel with N rooms
-- receive requests for bookings in an array
-- algorithm should return 
-  - 1 if can accomodate the set of bookings 
-  - 0 if N rooms are insufficient to accommodate bookins.
-
-============= STRATEGY: GREEDY ALGORITHM ==============
-- sort by smallest range in days
-- start couting with smallest booking for each day by storing the booking
-- if one day exceeds 3 the set of bookings cannot be done
-- if n never passes 3 to all the day its possible
-- by checking the shortest ranges first we cover a bigger number of booking in smallest time
-- if we are not greedy and get random booking the longest booking might take the time of 2 or more smaller bookings
-- in some cases the negative solutin might be found later
-Problem similar to:
-- processes scheduling time
-- line-up at groccery store
-*/
-
 #include <iostream>
 #include <string>
 #include <iterator>
 #include <vector>
 #include <algorithm>
 
+/*
+============= APPLICATION GOAL ==============
+Running a motel with N rooms
+- receive requests for bookings in an array
+- algorithm should return 
+  - 1 if can accomodate the set of bookings 
+  - 0 if N rooms are insufficient to accommodate bookings.
+*/
+
 using namespace std;
 
-// Driver function to sort the vector elements 
-// by second element of pairs 
 bool sortByShortestPeriod(const pair<int,int> &a, const pair<int,int> &b);
 void printVectorOfPairs(vector< pair <int, int> > vect, int n);
 int canBook(vector< pair <int, int> > vect, int n, int numRooms);
+int getMaxDate(vector< pair <int, int> > vect, int n);
+int getMinDate(vector< pair <int, int> > vect, int n);
 
 int main()
 {  
     // declaring vector of pairs 
-    vector< pair <int, int> > vect; 
+    vector< pair <int, int> > bookingDates; 
     // Initialising bookings
     int arrive_dates[] = {1,3,5,6,6};
     int leave_dates[] =  {2,6,8,7,8};
     int maxNumRooms = 3;
     int n = sizeof(arrive_dates)/sizeof(arrive_dates[0]); 
-    // Entering dates as pairs into vector
-    for (int i=0; i<n; i++) 
-        vect.push_back( make_pair(arrive_dates[i],leave_dates[i]) ); 
     
-    cout << canBook(vect,n, maxNumRooms) << "\n";
+    // Entering dates as pairs into vector
+    for (int i=0; i<n; i++) {
+        bookingDates.push_back( make_pair(arrive_dates[i],leave_dates[i]) ); 
+    }
+    
+    cout << "===============================================\n";
+    cout << canBook(bookingDates,n, maxNumRooms) << endl;
+    cout << "===============================================\n";
+    
+    maxNumRooms = 4; // same set of bookings, 1 extra room
+    cout << canBook(bookingDates,n, maxNumRooms) << endl;
+    
+
+    vector< pair <int, int> > bookingDates2;
+    int arrive_dates2[] = {1,5,8,1,3,5,7,10,1,6};
+    int leave_dates2[] =  {4,7,10,2,4,6,9,10,5,10};
+    maxNumRooms = 3;
+    n = sizeof(arrive_dates2)/sizeof(arrive_dates2[0]); 
+    
+    for (int i=0; i<n; i++) {
+        bookingDates2.push_back( make_pair(arrive_dates2[i],leave_dates2[i]) ); 
+    }
+
+    cout << "===============================================\n";
+    cout << canBook(bookingDates2,n, maxNumRooms) << endl;
+    cout << "===============================================\n";
 
     return 0;
 }
 
 int canBook(vector< pair <int, int> > vect, int n, int numRooms) {
-     //sort booking by smallest period
-     //printVectorOfPairs(vect, n);
-     sort(vect.begin(), vect.end(), sortByShortestPeriod); 
-     //printVectorOfPairs(vect, n);
-     //keep track of booked rooms (array)
-     //go through the sorted list of items
-     //increment number booked rooms
-    return 0;
+     
+    //sort booking by smallest period
+    sort(vect.begin(), vect.end(), sortByShortestPeriod); 
+
+    // vector to track total number of rooms booked in each day
+    int min =  getMinDate(vect, n);
+    int max =  getMaxDate(vect, n);
+    vector< pair <int, int> > bookedDays;
+
+    for (int i=min; i <= max; i++) {
+        bookedDays.push_back( make_pair(i,0));
+    }
+    
+    // iterate from smallest booking to largest
+    // and add days booked at each booking
+    for(int i = 0; i < n; i++)  {
+        
+        int j = vect[i].first;
+        
+        while(j <= vect[i].second) {
+            
+            bookedDays[j-1].second = (bookedDays[j-1].second) + 1;
+
+            if(bookedDays[j-1].second > numRooms) {
+                cout << "Set of bookings provided cannot be accomodated\n" 
+                     << "(day: " << bookedDays[j-1].first << " exceeds number of rooms)"  
+                     << endl;
+                return 0;
+            }
+            j++;
+        }
+    }
+    
+    cout << "Set of bookings provided can be accomodated" << endl;
+    return 1;
+}
+
+int getMinDate(vector< pair <int, int> > bookingDates, int n) {
+    
+    int min = bookingDates[0].first;
+    
+    for (int i = 1; i < n; i++){
+        if (bookingDates[i].first <  min) {
+                min = bookingDates[i].first;
+        } 
+    }
+
+    return min;
+}
+
+int getMaxDate(vector< pair <int, int> > bookingDates, int n) {
+    int max = bookingDates[0].second;
+    
+    for (int i = 1; i < n; i++){
+        if (bookingDates[i].second >  max) {
+                max = bookingDates[i].second;
+        } 
+    }
+
+    return max;
 }
 
 bool sortByShortestPeriod(const pair<int,int> &a, const pair<int,int> &b) 
@@ -67,10 +129,10 @@ bool sortByShortestPeriod(const pair<int,int> &a, const pair<int,int> &b)
     return ((a.second-a.first)+1 < (b.second-b.first)+1); 
 }
 
-void printVectorOfPairs(vector< pair <int, int> > vect, int n) {
-    cout << "The vector before sort operation is:\n" ; 
+void printVectorOfPairs(vector< pair <int, int> > bookingDates, int n) {
+    
     for (int i=0; i<n; i++) 
     { 
-        cout << vect[i].first << " " << vect[i].second << endl; 
+        cout << bookingDates[i].first << " " << bookingDates[i].second << endl; 
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # CPS 616 - Algorithms
 
-All the labs done for the Algorithms course (Winter 2019)
-
-### Lab 1 
+### Lab 1 - Sorting and Problem Size
 
 Sort arrays of different size using:
 - Selection Sort
@@ -10,22 +8,32 @@ Sort arrays of different size using:
 - A combination of Merge Sort and Selection Sort
   (Call Selection within Merge sort when size of the array is small enough)
 
-Measure the running time of each of the sorting algorithms
+Additionally, measure the running time of each of the sorting algorithms
 
-# Lab 2
+### Lab 2 - Divide and Conquer
 
 Implement your own **pow** function in:
 - linear time
 - lg n time
 
-## Lab 3
+### Lab 3 - Divide and Conquer
 
-implement number multiplication digit by digit using the following divide and conquer algorithms:
+Implement number multiplication digit by digit using the following divide and conquer algorithms:
 - multiply 4
 - multiply 3 (Karatsuba Algorithm)
 
-## Lab 4
+### Lab 4 - Graphs
 
 Given a graph as input, write an algorithm to determine a minimum spanning tree.
+- Minimum Spanning Tree determined using Prim's Algorithm.
+- Alternatively, Dijkstra's Algorithm could have been used.
 
-Minimum Spanning Tree determined using Prim's Algorithm.
+### Lab 5 - Greedy Algorithm
+
+Run a motel with N rooms. Determine if a set of bookings can be accomodated using a greedy algorithm.
+- Input: batches of bookings represented in arrays
+- Output: return 1 if able to accomodate set of bookings, 0 otherwise.
+
+Problem similar to the interval scheduling for processes in a operating system. In processes interval scheduling, we look for a algorithm that assigns a much number of processes as possible in the shortest period of time. To complete that, a greddy algorithm is implemented to obtain the shortest processes first.
+
+The same approach is used with the room accomodation, which sorts the bookings by shortest period and in a greedy way books the smallest bookings first. This allows to encounter a day that the number of rooms may be greater than the maximum number of rooms available sooner.   
